fcca6b955107afe10ef77ddaf98198473548f904
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-03-26 23:46:32 +0200

    ⏪ Changed directory name

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import RegisterForm from "../../Components/SignupForm/RegisterForm";
+import RegisterForm from "../../Components/RegisterForm/RegisterForm";
 
 export default function Register() {
   return <RegisterForm />;
