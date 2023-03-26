4dc68514a0492225022082018974d8dd0a4ffa6f
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-03-27 00:09:49 +0200

    ⏪ Change directory name

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -8,7 +8,6 @@ import { toast, ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.min.css";
 
-import Background from '../../assets/Images/signup.jpeg'
 
 export default function RegisterForm() {
   // BACKEND API
@@ -71,7 +70,7 @@ export default function RegisterForm() {
       <div className="flex justify-center items-center mt-28 mb-11">
         <div className="container mx-auto">
           <div className="bg-formColor flex flex-col lg:flex-row w-10/12 lg:w-8/12 rounded-xl mx-auto shadow-xl overflow-hidden">
-            <div style={{backgroundImage:`url(${Background})`}}  className="w-full h-[35rem] lg:w-1/2 bg-cover bg-center bg-no-repeat flex justify-center items-center">
+            <div  className="signupImg w-full h-[35rem] lg:w-1/2 bg-cover bg-center bg-no-repeat flex justify-center items-center">
               <div className="text-center">
                 <h1 className="text-white text-3xl">Welcome</h1>
                 <p className="text-white">
