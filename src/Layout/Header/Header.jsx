075861cead460e9812b1dfa9855d15c96b535ec8
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-04-01 03:29:04 +0200

    💄 Chanegd Blog title & icon

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -34,6 +34,19 @@ export default function Header() {
             className="normal-case text-2xl hover:text-primary font-extrabold"
           >
             ChillTime
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-7 h-7 inline ml-1 text-primary"
+            >
+              <path
+                strokeLinecap="round"
+                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
           </Link>
         </div>
 
@@ -54,7 +67,6 @@ export default function Header() {
 
           <div className="dropdown dropdown-end mr-4">
             <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
-
               <div className="rounded-full">
                 {gender === "Male" ? (
                   <img src="/male.svg" />
@@ -77,7 +89,6 @@ export default function Header() {
                   </svg>
                 )}
               </div>
-
             </label>
 
             <ul
